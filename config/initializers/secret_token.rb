00bc8d6d5edce70bb8663c01f8844bb8633eeef8
kahfei <bak.kahfei@gmail.com>
2012-02-28 23:50:21 +0800

    rake db:migrate locally

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lurelog::Application.config.secret_token = '07cc48f6c61b3c2c3e885d145b176ab1c2a749c19e11cf732b5b6597b02cede0b8e4ce02093f90071d9ab9a99277f0eb5b2a542502f50f834be841849f087d59'
